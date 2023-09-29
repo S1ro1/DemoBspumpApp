5cc72da17008349093015416de120034b1b78e2b
S1ro1 <matej.sirovatka@gmail.com>
2023-09-29 20:05:17 +0200

    Add generating values from normal distributions

--- utils/producer.py
+++ utils/producer.py
@@ -1,9 +1,21 @@
 from collections import defaultdict
+import dataclasses
+from pprint import pprint
 import random
 from kafka import KafkaProducer
 import asyncio
 import time
 import json
+import numpy as np
+from dataclasses import dataclass
+
+
+class DataclassEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if dataclasses.is_dataclass(obj):
+            return dataclasses.asdict(obj)
+        return super().default(obj)
+
 
 METRICS = [
     "free disk space",
@@ -34,6 +46,13 @@ HOST_NAMES = {
 }
 
 
+@dataclass
+class IdData:
+    id: str
+    mu: float
+    sigma: float
+
+
 def _setup_itemids(n: int) -> dict:
     ids = {}
     for customer, hosts in HOST_NAMES.items():
@@ -41,7 +60,11 @@ def _setup_itemids(n: int) -> dict:
             m2ids = defaultdict(list)
             for metric in METRICS:
                 for _ in range(n):
-                    m2ids[metric].append(str(random.randint(10**7, 10**8 - 1)))
+                    id = str(random.randint(10**7, 10**8 - 1))
+                    mu = random.uniform(0, 100)
+                    sigma = random.uniform(1, 10)
+
+                    m2ids[metric].append(IdData(id, mu, sigma))
 
             ids[f"{customer}-{host}"] = m2ids
 
@@ -61,13 +84,32 @@ class PeriodicSource:
 
         self.mapping = _setup_itemids(ids_per_metric)
 
+    def _generate_data(self) -> list[dict]:
+        samples = []
+        for host, metrics in self.mapping.items():
+            for metric_name, metric_ids in metrics.items():
+                for id_data in metric_ids:
+                    value = np.random.normal(id_data.mu, id_data.sigma)
+                    event = {
+                        "timestamp": int(time.time() * 1000),
+                        "host": host,
+                        "metric_name": metric_name,
+                        "itemid": id_data.id,
+                        "device": id_data.id,
+                        "value": value,
+                    }
+                    samples.append(event)
+
+        return samples
+
     async def produce(self):
         while True:
+            sample_events = self._generate_data()
             self.producer.send(self.topic, {"name": "new", "timestamp": int(time.time() * 1000)})
             await asyncio.sleep(self.interval)
 
 
 if __name__ == "__main__":
-    src = PeriodicSource("localhost:9093", "sample_input_topic", 1)
+    src = PeriodicSource("localhost:9093", "sample_input_topic", 30, ids_per_metric=3)
 
     asyncio.run(src.produce())
